d55243edf7ead75058a5ada0ae07603001c38c55
ArthuPereira <arthurpdsantoss@gmail.com>
2026-05-06 10:59:18 -0300

    feat(db): suporte a transactions

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -43,4 +43,24 @@ export class Database {
             await this.pool.end();
         }
     }
+
+    async transaction<T>(
+        fn: (client: PoolClient) => Promise<T>
+    ): Promise<T> {
+        const client = await this.getClient();
+
+        try {
+            await client.query("BEGIN");
+
+            const result = await fn(client);
+
+            await client.query("COMMIT");
+            return result;
+        } catch (error) {
+            await client.query("ROLLBACK");
+            throw error;
+        } finally {
+            client.release();
+        }
+    }
 }
